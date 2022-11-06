1854bce54fb71987e6358cda4433c47a3c61ca0e
Manoel Ribeiro <manoel.ribeiro@unilab.edu.br>
2022-11-06 13:12:42 -0300

    .

--- main.go
+++ main.go
@@ -2,66 +2,121 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-type No struct {
-	token       string
-	left, right *No
+func SliceToString(slice []string) string {
+	ret := ""
+	sep := ""
+	for x := range slice {
+		ret = ret + sep + slice[x]
+		sep = " "
+	}
+	return ret
 }
 
-func rnp(tokens []string) *No {
-	var tree *No
-
-	for _, x := range tokens {
-		switch x {
+func ToPrefixSingleLogic(exp string) (string, bool) {
+	s := strings.Split(exp, " ")
+	for x := range s {
+		switch s[x] {
 		case "and":
-			n := No{token: "min", left: tree}
-			tree = &n
+			fmt.Println(exp, "=> ", "math.Min( "+SliceToString(s[:x])+" , "+SliceToString(s[x+1:])+" )")
+			return "math.Min( " + SliceToString(s[:x]) + " , " + SliceToString(s[x+1:]) + " )", true
 		case "or":
-			n := No{token: "max", left: tree}
-			tree = &n
-		case "(":
-		case ")":
-		default:
-			n := No{token: x}
-			if tree == nil {
-				tree = &n
-			} else if tree.left == nil {
-				tree.left = &n
-			} else {
-				tree.right = &n
-			}
+			fmt.Println(exp, "=> ", "math.Max( "+SliceToString(s[:x])+" , "+SliceToString(s[x+1:])+" )")
+			return "math.Max( " + SliceToString(s[:x]) + " , " + SliceToString(s[x+1:]) + " )", true
 		}
 	}
-	return tree
+	return exp, false
 }
+func ToPrefix(exp string) string {
+	stack := make(map[string]string)
+	i_stack := 1000
+	key := "base"
+	stack[key] = exp
 
-func ExpRnp(n *No) string {
-	if n == nil {
-		return ""
-	} else {
-		return n.token + "(" + ExpRnp(n.left) + "," + ExpRnp(n.right) + ")"
+oulter:
+	for {
+		i := -1
+		j := -1
+		np := 0
+	inter:
+		for x := range stack[key] {
+			switch stack[key][x] {
+			case '(':
+				np++
+				if np == 1 {
+					i = x
+				}
+			case ')':
+				np--
+				if np == 0 {
+					j = x
+					break inter
+				}
+			}
+		}
+		if i != -1 {
+			i_stack++
+			k := fmt.Sprintf("zz%v", i_stack)
+			stack[k] = stack[key][i+1 : j]
+			stack[key] = strings.Replace(stack[key], "("+stack[k]+")", k, 1)
+			if !strings.Contains(stack[key], "(") {
+				todo := false
+			todo:
+				for k := range stack {
+					if strings.Contains(stack[k], "(") {
+						key = k
+						todo = true
+						break todo
+					}
+				}
+				if !todo {
+					break oulter
+				}
+			}
+
+		}
 	}
+
+	for {
+		var found bool
+		for key := range stack {
+			var f bool
+			stack[key], f = ToPrefixSingleLogic(stack[key])
+			found = found || f
+		}
+		if !found {
+			break
+		}
+
+	}
+	exp = stack["base"]
+	fmt.Println(exp)
+	for {
+		s := strings.Split(exp, " ")
+		found := false
+		for x := range s {
+			v, ok := stack[s[x]]
+			if ok {
+				exp = strings.Replace(exp, s[x], v, -1)
+				found = true
+			}
+		}
+		if !found {
+			break
+		}
+		fmt.Println(exp)
+	}
+	return exp
 }
 
-/*
-Crie uma pilha
-para cada caractere 't' no fluxo de entrada {
-se (t é um operando)
-saída t
-mais se (t é um parênteses certo){
-Tokens POP e de saída até que um parêntese esquerdo seja estourado (mas não faça saída)
-}
-outra coisa {
-Tokens POP e de saída até que um de menor prioridade do que t seja encontrado ou um parênteses esquerdos seja encontrado
-ou a pilha está vazia
-PUSH t
-}
-Fichas pop e de saída até que a pilha esteja vazia.
-*/
-
-// go get -u github.com/vinshop/exp-tree
+// Driver code
 func main() {
-	t := rnp([]string{"x1", "and", "(", "x2", "or", "x3", ")"})
-	fmt.PrintLn(ExpRnp(t))
+	s := ("x and (w or (a and b)) or (x and y)")
+
+	fmt.Println(s, ToPrefix(s))
+
+	//math.Max(math.Min(x, math.Max(w, math.Min(a, b)), math.Min(x, y)))
+
 }
